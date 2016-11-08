133e0482466d1b359d8a38c8eabd4eab7b525c9a
Salvador Hernández Mendoza <salvadorhm@gmail.com>
2016-11-08 17:07:31 -0600

    update connection

--- controllers/data.py
+++ controllers/data.py
@@ -4,7 +4,7 @@ render = web.template.render('views/',cache=False)
 
 class Data:
     def GET(self, records):
-        db = web.database(dbn='mysql', db='agenda', user='root', pw='toor')
+        db = web.database(dbn='mysql', host='ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' db='dj6fjvy0zpy56ntr', user='rpdu2zcvkh9rpllh', pw='dpgqepye9p3ucdlp')
         #db = PostgresqlDatabase(database='', user='',password='', host='', port='5432')
         records =[]
         records = db.select('contactos')
